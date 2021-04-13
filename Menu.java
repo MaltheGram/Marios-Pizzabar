65c942900f957d0e31d157500c7dfb17ba84a9c5
Mark Friis Larsen <marklarsen96@gmail.com>
2021-04-13 10:22:17 +0200

    markymark

---
+++ Menu.java
@@ -0,0 +1,8 @@
+package MariosPizza;
+
+import java.util.ArrayList;
+
+public class Menu {
+    ArrayList<String> pizzaMenu = new ArrayList<String>();
+    String orderNote;
+}
---
+++ Pizza.java
@@ -0,0 +1,13 @@
+package MariosPizza;
+
+public class Pizza {
+    public String pizzaName;
+    public String pizzaDescription;
+    public double pizzaPrice;
+
+    public Pizza (String pizzaName, String pizzaDescription, double pizzaPrice){
+        this.pizzaName = pizzaName;
+        this.pizzaDescription = pizzaDescription;
+        this.pizzaPrice = pizzaPrice;
+    }
+}
