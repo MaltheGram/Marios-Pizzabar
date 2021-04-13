8ae2c2c8b9a3fb945954403c20d238c362de6a8c
Mark Friis Larsen <marklarsen96@gmail.com>
2021-04-13 10:23:49 +0200

    markymark

--- PizzaMain.java
+++ PizzaMain.java
@@ -3,5 +3,6 @@ package MariosPizza;
 public class PizzaMain {
     public static void main(String[] args) {
         //do stuff
+        System.out.printf("%-50.50s %-50.50s%n","doot","shit");
     }
 }
